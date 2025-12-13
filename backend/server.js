2e22a2a1ddb51d42950e1324095257d406488f3d
shivamkalra123 <kalrashivam47@gmail.com>
2025-12-13 23:15:53 +0530

    admin

--- backend/server.js
+++ backend/server.js
@@ -1,51 +1,39 @@
-import express from "express"
+import express from "express";
 import cors from "cors";
-import "dotenv/config"
+import "dotenv/config";
 import connectDB from "./config/mongodb.js";
 import connectCloudinary from "./config/cloudinary.js";
 import userRouter from "./routes/userRoute.js";
 import productRouter from "./routes/productRoute.js";
 import cartRouter from "./routes/cartRoute.js";
 import orderRouter from "./routes/orderRoute.js";
-import categoriesRoute from "./routes/categoryRoute.js"
-import bannerRoute from "./routes/bannerRoute.js"
+import categoriesRoute from "./routes/categoryRoute.js";
+import bannerRoute from "./routes/bannerRoute.js";
 
-
-//app congig
 const app = express();
 const port = process.env.PORT || 4000;
-connectDB()
-connectCloudinary()
 
-app.use(
-  cors({
-    origin: "*",
-    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-    credentials: false,
-  })
-);
+connectDB();
+connectCloudinary();
 
-//middleware
-app.use(express.json())
-app.use(cors())
 
-app.use('/api/categories', categoriesRoute);
 
-//api endpoints
-app.use('/api/user',userRouter)
-app.use('/api/product',productRouter)
-app.use("/api/cart",cartRouter)
-app.use("/api/order",orderRouter)
+app.use(express.json());
+
+// routes
+app.use("/api/categories", categoriesRoute);
+app.use("/api/user", userRouter);
+app.use("/api/product", productRouter);
+app.use("/api/cart", cartRouter);
+app.use("/api/order", orderRouter);
 app.use("/api/banner", bannerRoute);
 
-app.get("/",(req,res)=>{
-    res.send("API working")
-})
+app.get("/", (req, res) => {
+  res.send("API working");
+});
 
-app.listen(port,()=>{
-    console.log(`Server started on PORT: ${port}`);
-    
-})
+app.listen(port, () => {
+  console.log(`Server started on PORT: ${port}`);
+});
 
 export default app;
