cf9796f94fd22712bf55fe717b2cd3caf093ad48
shivamkalra123 <kalrashivam47@gmail.com>
2025-12-14 14:52:25 +0530

    server

--- backend/server.js
+++ backend/server.js
@@ -37,7 +37,11 @@ app.use((req, res, next) => {
   }
 
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+res.setHeader(
+  "Access-Control-Allow-Headers",
+  "Content-Type, Authorization, token"
+);
+
   res.setHeader("Access-Control-Allow-Credentials", "true");
 
   if (req.method === "OPTIONS") {
